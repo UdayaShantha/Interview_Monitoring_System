5d311947c4fc508488f91faf80f230322a69638e
nadeesha <anadeesha9@gmail.com>
2024-12-20 10:06:42 +0530

    create backend services

---
+++ backend/services/question-management-service/src/test/java/com/aipoweredinterviewmonitoringsystem/question_management_service/QuestionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipoweredinterviewmonitoringsystem.question_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
