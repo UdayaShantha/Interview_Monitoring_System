5410535f5f34c8ff01a19eef7d7426ee0ca25dbb
nadeesha <anadeesha9@gmail.com>
2024-12-22 18:30:30 +0530

    create question and answer entities in question management service

---
+++ backend/services/question-management-service/src/main/java/com/aipoweredinterviewmonitoringsystem/question_management_service/entity/Solution.java
@@ -0,0 +1,36 @@
+package com.aipoweredinterviewmonitoringsystem.question_management_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "solutions")
+public class Solution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "solution_id")
+    private Long solutionId;
+
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "created_by", nullable = false)
+    private Long createdBy;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", insertable = false, updatable = false)
+    private Question question;
+}
