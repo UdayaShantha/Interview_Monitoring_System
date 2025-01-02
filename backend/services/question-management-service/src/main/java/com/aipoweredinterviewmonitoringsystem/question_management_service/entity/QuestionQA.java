d45b80fe2b4ea58ef30a6a21ed239e1515eee1df
Neudari <neudara21@gmail.com>
2025-01-02 21:52:56 +0530

    basic CRUD operations for report generation and question management services

---
+++ backend/services/question-management-service/src/main/java/com/aipoweredinterviewmonitoringsystem/question_management_service/entity/QuestionQA.java
@@ -0,0 +1,32 @@
+package com.aipoweredinterviewmonitoringsystem.question_management_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "question_QA")
+
+public class QuestionQA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_qa_id")
+    private Long questionQAId;
+
+    @OneToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "duration" ,nullable = false)
+    private LocalTime duration;
+
+    public void setQuestionQAId(Long id) {
+        this.questionQAId = id;
+    }
+}
---
+++ backend/services/question-management-service/src/main/java/com/aipoweredinterviewmonitoringsystem/question_management_service/entity/QuestionSE.java
@@ -0,0 +1,33 @@
+package com.aipoweredinterviewmonitoringsystem.question_management_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "question_SE")
+
+public class QuestionSE {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_se_id")
+    private Long questionSEId;
+
+    @OneToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "duration" ,nullable = false)
+    private LocalTime duration;
+
+    public void setQuestionSEId(Long id) {
+        this.questionSEId = id;
+    }
+
+}
