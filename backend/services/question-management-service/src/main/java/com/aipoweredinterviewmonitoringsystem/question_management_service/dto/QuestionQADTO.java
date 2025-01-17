64d8ef072d820bdda12849cb448696ecbdfc1290
Udaya <udayashanaka42@gmail.com>
2025-01-17 15:22:18 +0530

    create question management DTOs

---
+++ backend/services/question-management-service/src/main/java/com/aipoweredinterviewmonitoringsystem/question_management_service/dto/QuestionQADTO.java
@@ -0,0 +1,19 @@
+package com.aipoweredinterviewmonitoringsystem.question_management_service.dto;
+
+import com.aipoweredinterviewmonitoringsystem.question_management_service.entity.Question;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class QuestionQADTO {
+    private Question question;
+    private Long duration;
+    private String content;
+    private List<String> qaKeywords;
+}
