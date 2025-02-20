739d715b209a07bd2e6dc245ce815f0af3c56b75
DhananjayaYN <dhananjayanimesh17@gmail.com>
2025-02-20 11:06:54 +0530

    Configure the FrontEnd environment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>React project with TailwindCSS</h1>
+        
+    </>
+  )
+}
+
+export default App
