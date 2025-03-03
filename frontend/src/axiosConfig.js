b5ee5dbb463b00df68bf6b8b5841ff7b19666040
Neudari <neudara21@gmail.com>
2025-03-03 11:39:59 +0530

    creating axios

---
+++ frontend/src/axiosConfig.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:9191/api/v1"; 
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
