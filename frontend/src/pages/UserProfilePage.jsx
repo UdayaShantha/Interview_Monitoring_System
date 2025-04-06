64ff459fe785548ddb7b6902742f662e8352ca20
Nadeesha Abeywickrama <127606065+NadeeshaAbey@users.noreply.github.com>
2025-04-06 12:45:38 +0530

    handle logout functionality in interview page and user profile page

--- frontend/src/pages/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage.jsx
@@ -35,6 +35,8 @@ function UserProfilePage() {
   }, []);
 
   function handleLogout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
     navigate('/login');
   }
 
